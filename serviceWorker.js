fb8bc92cf511ba5685a136012267ee206f54e8d2
DARKNEET69 <fierce1371@gmail.com>
2023-05-06 21:05:09 +0300

    Update SEO

--- serviceWorker.js
+++ serviceWorker.js
@@ -10,7 +10,7 @@ const {
     precaching: { matchPrecache, precacheAndRoute },
 } = workbox;
 
-precacheAndRoute([{ url: "https://darkneet69.github.io/universal-character-sheet/offline.html", revision: null }]);
+precacheAndRoute([{ url: "https://darkneet69.github.io/character.sheet.online/offline.html", revision: null }]);
 
 // Cache page navigations (html) with a Network First strategy
 registerRoute(
@@ -79,7 +79,7 @@ setCatchHandler(async ({ event }) => {
     /*
     // Return the precached offline page if a document is being requested
     if (event.request.destination === "document") {
-        return matchPrecache("https://darkneet69.github.io/universal-character-sheet/offline.html");
+        return matchPrecache("https://darkneet69.github.io/character.sheet.online/offline.html");
     }
 
     return Response.error();
